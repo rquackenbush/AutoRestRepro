3b60436e758637f3b7da3c8661656e3152f81776
Rich Quackenbush <Richard.Quackenbush@AcuityBrands.com>
2023-05-30 09:36:35 -0400

    This works!

--- MathClient/Customized/Models/AddRequest.cs
+++ MathClient/Customized/Models/AddRequest.cs
@@ -1,10 +1,36 @@
 ﻿using Azure.Core;
+using System.Text.Json;
+
+[assembly: CodeGenSuppressType("AddRequest")]
 
 namespace MathClient.Models
 {
-    public partial class AddRequest
+    public partial class AddRequest : IUtf8JsonSerializable
     {
         [CodeGenMember("Values")]
         public IList<int> Values { get; set; }
+
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        {
+            writer.WriteStartObject();
+            if (Optional.IsCollectionDefined(Values))
+            {
+                if (Values != null)
+                {
+                    writer.WritePropertyName("values"u8);
+                    writer.WriteStartArray();
+                    foreach (var item in Values)
+                    {
+                        writer.WriteNumberValue(item);
+                    }
+                    writer.WriteEndArray();
+                }
+                else
+                {
+                    writer.WriteNull("values");
+                }
+            }
+            writer.WriteEndObject();
+        }
     }
 }
